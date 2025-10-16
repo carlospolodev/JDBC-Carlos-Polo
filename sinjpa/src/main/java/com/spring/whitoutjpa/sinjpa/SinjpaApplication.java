663fa7ff5f190a0583341b13e03daef68ac13337
johan0920-sketch <johan0920@gmail.com>
2025-10-16 12:42:15 -0500

    Codigo Inicial

---
+++ sinjpa/src/main/java/com/spring/whitoutjpa/sinjpa/SinjpaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.whitoutjpa.sinjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinjpaApplication.class, args);
+	}
+
+}
---
+++ sinjpa/src/test/java/com/spring/whitoutjpa/sinjpa/SinjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.whitoutjpa.sinjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
